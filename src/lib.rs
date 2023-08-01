8d6f1bbe2acc6b0f2e1b643fdd9d98adbfd51c8e
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2023-08-01 05:22:59 -0400

    Add format_deposit_address function

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
-pub mod types;
-pub mod wallet;
-pub mod node;
-pub mod miner;
-pub mod state;
-pub mod net;
 pub mod archive;
-pub mod mempool;
 pub mod authorization;
 pub mod drivechain;
-pub use heed;
+pub mod mempool;
+pub mod miner;
+pub mod net;
+pub mod node;
+pub mod state;
+pub mod types;
+pub mod wallet;
 pub use bitcoin;
+pub use heed;
+pub use jsonrpsee;
+
+/// Format `str_dest` with the proper `s{sidechain_number}_` prefix and a
+/// checksum postfix for calling createsidechaindeposit on mainchain.
+pub fn format_deposit_address(str_dest: &str) -> String {
+    let this_sidechain = 0;
+    let deposit_address: String = format!("s{}_{}_", this_sidechain, str_dest);
+    let hash = sha256::digest(deposit_address.as_bytes()).to_string();
+    let hash: String = hash[..6].into();
+    format!("{}{}", deposit_address, hash)
+}
