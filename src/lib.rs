0cb961b678b334f4990bfa36ed45021ffffa7f7a
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2023-08-03 10:45:52 -0400

    Don't hardcode slot num in format_deposit_address

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,7 @@ pub use jsonrpsee;
 
 /// Format `str_dest` with the proper `s{sidechain_number}_` prefix and a
 /// checksum postfix for calling createsidechaindeposit on mainchain.
-pub fn format_deposit_address(str_dest: &str) -> String {
-    let this_sidechain = 0;
+pub fn format_deposit_address(this_sidechain: u8, str_dest: &str) -> String {
     let deposit_address: String = format!("s{}_{}_", this_sidechain, str_dest);
     let hash = sha256::digest(deposit_address.as_bytes()).to_string();
     let hash: String = hash[..6].into();
